37cf12d8bf720c90731e39fc923d0ec4d858d340
peterlidee <peter@lidee.be>
2026-05-19 17:04:55 +0200

    Added example routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,45 @@
-export default function HomePage() {
-  return <div>Hello world!</div>;
+import Link from 'next/link';
+
+const chapters = [
+  {
+    title: 'Chapter 12: PPR',
+    links: [
+      { label: 'example with connection', route: '/chapter-12/connection' },
+      {
+        label: 'dynamic component with child client component',
+        route: '/chapter-12/dynamic-with-client-child',
+      },
+      { label: 'example with searchParams', route: '/chapter-12/searchparams' },
+    ],
+  },
+];
+
+export default function Home() {
+  return (
+    <>
+      <h1 className='text-xl font-bold mb-4'>home</h1>
+      <div>
+        {chapters.map((chapter) => {
+          return (
+            <div key={chapter.title} className='mb-4'>
+              <h2>{chapter.title}</h2>
+              <ul>
+                {chapter.links.map((link) => (
+                  <li key={link.route}>
+                    <Link
+                      href={link.route}
+                      className='underline text-blue-500'
+                      // prefetch={false}
+                    >
+                      {link.label}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
